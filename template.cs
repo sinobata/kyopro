d030113746103de02def2188c468aa23153ab195
Tabata Ryo <batabata4096@gmail.com>
2022-08-16 17:48:48 +0900

    init

---
+++ template.cs
@@ -0,0 +1,47 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Utils
+    {
+        
+    }
+}
+
+// 文字列の入力
+// string s = Console.ReadLine();
+
+// 整数の入力
+// long n = long.Parse(Console.ReadLine());
+
+// 文字列配列の入力
+// string[] inputStrArray = Console.ReadLine().Split(' ');
+
+// 整数配列の入力
+// long[] inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+// int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+// 配列を生成する
+// var array = new int[] {0, 2, 4, 6};
+
+// [0,1...] の配列を生成する
+// var array = Enumerable.Range(0, 5).ToArray(); // {0, 1, 2, 3, 4}
+
+// 初期値が全て同じ配列を生成する
+// var array = Enumerable.Repeat(-1, 5).ToArray(); // { -1, -1, -1, -1, -1}
+
+// ソート
+// Array.Sort(array);
+
